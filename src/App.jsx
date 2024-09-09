698c31227922e8f7646e89a28ecd5b26bde6b0d6
Emma Gonin <emmagonin1@gmail.com>
2024-09-09 10:51:34 +0200

    First home view + header styled

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Home from "./components/Home";
+import Portfolio from "./components/Portfolio";
+import Gallery from "./components/Gallery";
+import AboutMe from "./components/AboutMe";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+      mode: 'dark',
+  },
+});
+
+const App = () => {
+  const [selectedView, setSelectedView] = useState("home");
+  const renderContent = () => {
+    switch (selectedView) {
+      case "home":
+        return <Home />;
+      case "portfolio":
+        return <Portfolio />;
+      case "gallery":
+        return <Gallery />;
+      case "about me":
+        return <AboutMe />;
+      case "contact":
+        return <Contact />;
+      default:
+        return null;
+    }
+  };
+  return (
+  <ThemeProvider theme={theme}>
+   <Header selectedView={selectedView} />
+   <main>{renderContent()}</main>
+  </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./home.css";
+import Header from "./Header";
+
+function Home() {
+
+  return (
+    <div className="background-container">
+        {/* <Header/> */}
+        <div className="abril-fatface-regular" style={{ color: 'white', fontSize: '110px' }}>
+            Emma Casagrande</div>
+            <div className="abril-fatface-regular" style={{ color: 'white', fontSize: '60px' }}>
+            peintures</div>
+  </div>
+)
+}
+
+
+export default Home;
