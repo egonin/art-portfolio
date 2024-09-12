d75f4e382ab0a6c7a1bc3071f7d6f7866839e43a
Emma Gonin <emmagonin1@gmail.com>
2024-09-12 19:02:03 +0200

    BugFix, V1.0 Du portfolio !

--- src/components/ImageDisplay.jsx
+++ src/components/ImageDisplay.jsx
@@ -7,7 +7,14 @@ const ImageDisplay = ({ src, alt }) => {
   console.log(src)
   const [imageSrc, setImageSrc] = useState(src);
   console.log(imageSrc)
+  console.log('File type:', src.type);
+  // const isImageFile = (file) => {
+  //   const imageTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'image/bmp', 'image/svg+xml'];
+  //   return file && imageTypes.includes(file.type);
+  // };
 
+  // console.log( isImageFile(src), "true if is an image, false if not")
+  
   useEffect(() => {setImageSrc(src)
   },[src]
   )
@@ -29,11 +36,12 @@ const ImageDisplay = ({ src, alt }) => {
     <Box>
       {src && (
         <img 
-          src={src} 
-          alt={alt} 
-        //   onLoad={handleImageLoad}
+        src={src} 
+        alt={alt} 
+      //   onLoad={handleImageLoad}
         style={{ width: 'auto', height: '75vh', objectFit: 'contain' }}
-        />
+        onError={(e) => console.error('Erreur de chargement de l\'image:', e)}
+      />
       )}
     </Box>
   );
