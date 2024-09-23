8f7999cb74ebea4e512ae40f69448f79ecdf8418
Emma Gonin <emmagonin1@gmail.com>
2024-09-23 11:13:50 +0200

    galerie photo !! :)

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import "./home.css";
+import { ImageList, ImageListItem, Box } from '@mui/material'
+import data from "../data/data.json"
 
 function Gallery({ selectedView }) {
 
   return (
     <div className="background-container">
-        <div className="abril-fatface-regular" style={{ color: 'white', fontSize: '110px' }}>
-            work in progress</div>
-  </div>
-)
+      <Box sx={{ overflowY: 'scroll' }}>
+      <ImageList variant="masonry" cols={3} gap={8}>
+        {data.paintings.map((item) => (
+          <ImageListItem key={item.img_url}>
+            <img
+              srcSet={`${item.img_url}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              src={`${item.img_url}?w=248&fit=crop&auto=format`}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+      </Box>
+    </div>
+  )
 }
 
 
