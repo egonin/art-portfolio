39e08130efd2c2df09792f6778f8dfb919e8899b
Emma Gonin <emmagonin1@gmail.com>
2024-11-11 12:46:47 +0100

    préchargement des images pour la galerie

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,12 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./home.css";
-import { ImageList, ImageListItem, Box } from '@mui/material'
+import { ImageList, ImageListItem, Box, Typography } from '@mui/material'
 import data from "../data/data.json"
 
 function Gallery({ selectedView }) {
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [visibleItems, setVisibleItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const preloadImages = async () => {
+      const visibleItemIndices = [currentIndex, (currentIndex + 1) % data.paintings.length];
+      
+      const preloadPromises = visibleItemIndices.map(index => 
+        loadImage(data.paintings[index])
+      );
+
+      await Promise.all(preloadPromises);
+
+      setIsLoading(false);
+      setVisibleItems(visibleItemIndices.map(index => data.paintings[index]));
+    };
+
+    preloadImages();
+  }, [currentIndex]);
+
+  const loadImage = async (item) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = () => reject(new Error(`Failed to load image: ${item.img_url}`));
+      img.src = item.img_url;
+    });
+  };
+
+  useEffect(() => {
+    if (!isLoading) {
+      console.log("All images loaded and visibleItems updated");
+    }
+  }, [isLoading]);
+
   return (
     <div className="background-container">
+      {isLoading ? (
+        <div/>
+      ) : (
       <Box sx={{ overflowY: 'scroll' }}>
       <ImageList variant="masonry" cols={3} gap={8}>
         {data.paintings.map((item) => (
@@ -21,6 +60,7 @@ function Gallery({ selectedView }) {
         ))}
       </ImageList>
       </Box>
+    )}
     </div>
   )
 }
